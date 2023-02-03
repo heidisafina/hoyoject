b20f94696a18c05dda6409e71b4d4ac52873e304
heidisafina <107091232+heidisafina@users.noreply.github.com>
2023-02-03 13:21:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php include 'include/db.php';?>
+
+<!-- Header -->
+<?php include 'include/header.php';?>
+
+<!-- Navigation Bar -->
+<?php include 'include/navigation.php';?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+                
+                   <?php 
+                   if(isset($_GET['page'])){
+                    $page = $_GET['page'];
+                   }else{
+                    $page = ""; 
+                   }
+                   if($page == "" || $page == 1){
+                    $page_1 = 0;
+                   } else{
+                    $page_1 = ($page * 3) - 3;  
+                   }
+
+                   $post_query_count = "SELECT * FROM posts";
+                   $find_count = mysqli_query($connection, $post_query_count);
+                   $count = mysqli_num_rows($find_count);
+                   $count = ceil($count / 3);
+                    
+                    $query = "SELECT * FROM posts  LIMIT $page_1, 3";   
+                    $select_all_posts_query = mysqli_query($connection, $query);
+                
+                    while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                        $post_id = $row['post_id'];
+                        $post_title = $row['post_title'];  
+                        $post_user = $row['post_user'];  
+                        $post_date = $row['post_date'];  
+                        $post_image = $row['post_image'];
+                        $post_content = substr($row['post_content'],0,100);  
+                        $post_status = $row['post_status'];
+                        if($post_status == 'draft'){
+                       
+                        } else {
+                        ?>
+                        <h1 class="page-header">
+                        Ad Astra Abyssosque
+                        <small>welcome Traveller</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                        <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?>
+                        </a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="author_posts.php?author=<?php echo $post_user;?>&p_id=<?php echo $post_id;?>"><?php echo $post_user;?></a>
+                        </p>
+                        <p>
+                        <span class="glyphicon glyphicon-time"></span><?php echo $post_date;?>
+                        </p>
+                        <hr>
+                        <a href="post.php?p_id=<?php echo $post_id;?>">
+                        <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+                        </a>
+                        <hr>
+                        <p><?php echo $post_content;?></p>
+                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id;?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <hr>
+                
+                    <?php } }?> 
+                        <ul class="pager">
+                            <?php
+                                for($i =1; $i <= $count; $i++){
+                                    if($i == $page){
+                                        echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
+                                    }else{
+                                        echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                                    }
+                                }
+                            ?>
+                        </ul>
+                        
+            </div>
+            <!-- Sidebar Widgets Column -->
+            <?php include 'include/sidebar.php';?>
+
+            <!-- Footer -->
+            <?php include 'include/footer.php';?>
